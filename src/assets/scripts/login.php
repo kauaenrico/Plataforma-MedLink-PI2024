b87d9f67c697855c0f23501f85a6f4edbaa8967e
Kauã Enrico <kaua.altran@outlook.com>
2024-03-24 16:02:44 -0300

    mudanças login

--- src/assets/scripts/login.php
+++ src/assets/scripts/login.php
@@ -1,6 +1,6 @@
 <?php
 // Conexão com o banco de dados
-$servername = "10.0.0.20"; // Endereço IP do seu servidor MySQL
+$servername = "db01.ddnsfree.com"; // Endereço IP do seu servidor MySQL
 $username = "admin"; // Nome de usuário do MySQL
 $password = "unisal@2024"; // Senha do MySQL
 $dbname = "plataforma"; // Nome do seu banco de dados
