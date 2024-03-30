d514c36d594431c4b0d66ae7f3a498e37da8004d
Kauã Enrico <kaua.altran@outlook.com>
2024-03-30 17:32:19 -0300

    Update phpteste.php

--- src/teste-login/phpteste.php
+++ src/teste-login/phpteste.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 <body>
     <?php 
     $value = 1; // Just testing
@@ -8,3 +15,7 @@
         <h1>Login was successful.</h1>
     ?>
 </body>
+</html>
+
+
+
