916e4f229d36985853abbd04c11a064b172349d8
Kauã Enrico <kaua.altran@outlook.com>
2024-03-30 17:47:12 -0300

    teste php

--- src/teste-login/phpteste.php
+++ src/teste-login/phpteste.php
@@ -1,28 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teste PHP</title>
-</head>
-
-<body>
-    <form method="post" action="phpteste.php">
-        <label for="name">Nome:</label>
-        <input type="text" id="name" name="name">
-        <br>
-        <label for="email">E-mail:</label>
-        <input type="email" id="email" name="email">
-        <br>
-        <button type="submit" name="submit">Enviar</button>
-    </form>
-
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        header("Location: index.php");
-    }
-    ?>
-</body>
-
-</html>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    header("Location: index.php");
+}
+?>
