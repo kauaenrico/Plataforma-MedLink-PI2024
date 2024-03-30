10d6e9cb560a3969935a444e1ff9b2d0a85bc1ff
Kauã Enrico <kaua.altran@outlook.com>
2024-03-30 01:27:58 -0300

    Update login.php

--- src/teste-login/login.php
+++ src/teste-login/login.php
@@ -21,11 +21,11 @@ if (isset($_SESSION["user"])) {
            $email = $_POST["email"];
            $password = $_POST["password"];
             require_once "database.php";
-            $sql = "SELECT * FROM users WHERE email = '$email'";
+            $sql = "SELECT * FROM profissionais WHERE profiss_email = '$email'";
             $result = mysqli_query($conn, $sql);
             $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
             if ($user) {
-                if (password_verify($password, $user["password"])) {
+                if (password_verify($password, $user["profiss_passwd"])) {
                     session_start();
                     $_SESSION["user"] = "yes";
                     header("Location: index.php");
