93c2298eb913ba11047b422d7e5d4e307595af0d
Kauã Enrico <kaua.altran@outlook.com>
2024-03-31 21:35:58 -0300

    Revert "Update dashboard.php"
    This reverts commit 1dc21032fc74288acbe42586f153dd7e0b4f6b11.

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,9 +1,9 @@
-#<?php
-#session_start();
-#if (!isset($_SESSION["user"])) {
-#   header("Location: login.php");
-#}
-#?>
+<?php
+session_start();
+if (!isset($_SESSION["user"])) {
+   header("Location: login.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
