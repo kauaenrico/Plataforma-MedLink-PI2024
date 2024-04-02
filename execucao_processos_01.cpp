4f50fa9efb15cc043c610b60ca9027a394e5d4cf
Kauã Enrico <kaua.altran@outlook.com>
2024-04-02 19:40:29 -0300

    Create execucao_processos_01.cpp

---
+++ execucao_processos_01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <omp.h>
+#include <locale.h>
+using namespace std;
+
+int main() {
+    system("cls");
+    setlocale(LC_ALL, "portuguese");
+    const int tamanho = 10; //tamanho do vetor
+    int vetor[tamanho];
+    int soma = 0;
+
+    //preenchendo o vetor
+    for (int i = )
+
+
+
+}
