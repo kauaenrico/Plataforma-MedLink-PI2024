6a802ad77625268786804c02d21aec493642ddb8
Kauã Enrico <kaua.altran@outlook.com>
2024-04-22 21:48:00 -0300

    Update dashboard.php

--- src/dashboard.php
+++ src/dashboard.php
@@ -37,10 +37,6 @@
           <a class="nav-link" href="#">Administração</a>
         </li>
       </ul>
-      <form class="d-flex">
-        <input class="form-control me-sm-2" type="search" placeholder="Search">
-        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
