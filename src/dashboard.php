6970e6d8308636c9d707bddb63721d3b688fcc19
Kauã Enrico <kaua.altran@outlook.com>
2024-04-22 21:57:38 -0300

    header

--- src/dashboard.php
+++ src/dashboard.php
@@ -8,40 +8,10 @@
 </head>
 <body>
     <header>
-    <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">MedLink</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarColor01">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Pagina Inicial
-            <span class="visually-hidden">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pacientes</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Agenda</a>
-          <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Minha Agenda</a>
-            <a class="dropdown-item" href="#">Nova Sessão</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Separated link</a>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="error404.html?page=erro">Administração</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+        <?php
+            include "header.php";
+        ?>
     </header>
-
     <main class="h-screen flex flex-col items-center justify-center py-2">
         <?php
             if(isset($_GET["page"])){
