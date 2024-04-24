576a9fc09b3fc8daec93e3a6f2b2eb365d2be3db
LukeBelatine <luke.belatine@yahoo.com.br>
2024-04-23 21:40:09 -0300

    Update dashboard.php

--- src/dashboard.php
+++ src/dashboard.php
@@ -97,9 +97,9 @@
                                 <h5 class="card-title">Novos brinquedos nas salas!</h5>
                                 <h6 class="card-subtitle text-muted">Novas experiencias para novos resultados</h6>
                             </div>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="d-block user-select-none" width="70%" height="140" aria-label="Placeholder: Image cap" focusable="false" role="img" preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180" style="font-size:1.125rem;text-anchor:middle">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="d-block user-select-none" width="100%" height="200" aria-label="Placeholder: Image cap" focusable="false" role="img" preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180" style="font-size:1.125rem;text-anchor:middle">
                                 <rect width="100%" height="100%" fill="#868e96"></rect>
-                                <text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>
+                                <text x="100%" y="100%" fill="#dee2e6" dy=".3em">Image cap</text>
                             </svg>
                             <div class="card-body">
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
