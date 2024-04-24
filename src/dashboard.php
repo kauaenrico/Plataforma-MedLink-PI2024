30c4a78f13f07eb5757fa3dfa347f84366ad96b9
LukeBelatine <luke.belatine@yahoo.com.br>
2024-04-23 21:44:42 -0300

    Update dashboard.php

--- src/dashboard.php
+++ src/dashboard.php
@@ -97,8 +97,8 @@
                                 <h5 class="card-title">Novos brinquedos nas salas!</h5>
                                 <h6 class="card-subtitle text-muted">Novas experiencias para novos resultados</h6>
                             </div>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="d-block user-select-none" width="200%" height="400" aria-label="Placeholder: Image cap" focusable="false" role="img" preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180" style="font-size:1.125rem;text-anchor:middle">
-                                <rect width="200%" height="200%" fill="#868e96"></rect>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="d-block user-select-none" width="100%" height="200" aria-label="Placeholder: Image cap" focusable="false" role="img" preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180" style="font-size:1.125rem;text-anchor:middle">
+                                <rect width="500%" height="200%" fill="#868e96"></rect>
                                 <text x="100%" y="100%" fill="#dee2e6" dy=".3em">Image cap</text>
                             </svg>
                             <div class="card-body">
