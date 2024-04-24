2b5e01de81b45c5b7c17b821e6ff4a2667bbf35e
LukeBelatine <luke.belatine@yahoo.com.br>
2024-04-23 21:36:01 -0300

    Update dashboard.php

--- src/dashboard.php
+++ src/dashboard.php
@@ -92,12 +92,12 @@
                 <div class="row">
                     <div class="col-md-6">
                         <div class="card mb-3">
-                            <h3 class="card-header">Card header</h3>
+                            <h3 class="card-header">Novidades</h3>
                             <div class="card-body">
-                                <h5 class="card-title">Special title treatment</h5>
-                                <h6 class="card-subtitle text-muted">Support card subtitle</h6>
+                                <h5 class="card-title">Novos brinquedos nas salas!</h5>
+                                <h6 class="card-subtitle text-muted">Novas experiencias para novos resultados</h6>
                             </div>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="d-block user-select-none" width="100%" height="200" aria-label="Placeholder: Image cap" focusable="false" role="img" preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180" style="font-size:1.125rem;text-anchor:middle">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="d-block user-select-none" width="70%" height="140" aria-label="Placeholder: Image cap" focusable="false" role="img" preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180" style="font-size:1.125rem;text-anchor:middle">
                                 <rect width="100%" height="100%" fill="#868e96"></rect>
                                 <text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>
                             </svg>
