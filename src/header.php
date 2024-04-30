3b6a499ee2df2da072d6d031edb992e1f164a270
Kauã Enrico <kaua.altran@outlook.com>
2024-04-30 19:31:00 -0300

    Update header.php

--- src/header.php
+++ src/header.php
@@ -7,7 +7,7 @@
     <div class="collapse navbar-collapse" id="navbarColor01">
       <ul class="navbar-nav me-auto">
         <li class="nav-item">
-          <a class="nav-link active" href="dashboard.php?page=erro">Pagina Inicial<span class="visually-hidden">(current)</span>
+          <a class="nav-link active" href="index.php?page=dashboard">Pagina Inicial<span class="visually-hidden">(current)</span>
           </a>
         </li>
         <li class="nav-item">
