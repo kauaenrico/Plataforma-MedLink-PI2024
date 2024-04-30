78c73c3fb513ee216188d692212ae867e9480d3b
Kauã Enrico <kaua.altran@outlook.com>
2024-04-30 19:40:56 -0300

    Update header.php

--- src/header.php
+++ src/header.php
@@ -28,6 +28,9 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Administração</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?page=gitpull">Force Git Pull</a>
+        </li>
       </ul>
     </div>
   </div>
