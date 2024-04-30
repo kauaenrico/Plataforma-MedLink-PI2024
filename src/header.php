845f0f6c4623e5ba8d5500265a974de71bf954a6
Kauã Enrico <kaua.altran@outlook.com>
2024-04-30 20:11:12 -0300

    Update header.php

--- src/header.php
+++ src/header.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">MedLink</a>
+    <a class="navbar-brand" href="index.php?page=dashboard">MedLink</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
