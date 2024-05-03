fcec250824415a535f4119ac1569ac4f6a4ecccd
LukeBelatine <luke.belatine@yahoo.com.br>
2024-05-03 19:58:05 -0300

    Update dashboard.php

--- src/dashboard.php
+++ src/dashboard.php
@@ -68,19 +68,19 @@
                     </div>
                     <img src="C:\Users\luke.belatine\OneDrive - Centro de Tecnologia Canavieira SA\Área de Trabalho\Unisal_5BI\Imagemclinica.jpeg" width="100%" height="200" alt="Imagem da clínica" style="object-fit: cover;">
                     <div class="card-body">
-                        <p class="card-text">Novos jogos e brinquedos que nos possibilitarão um auxílio maior no desenvolvimento de nossos pacientes.</p>
+                        <p class="card-text">Novos jogos e brinquedos que nos possibilitarão um auxílio maior no desenvolvimento de nossos pacientes. veja exemplos de alguns novos brinquedos que chegaram ao PequenosPassos</p>
                         <!-- Link "Saiba mais" -->
                         <a href="#" class="btn btn-primary" id="saibaMaisBtn">Saiba mais</a>
                         <!-- Div oculta com informações adicionais -->
                         <div id="infoAdicionais" style="display: none;">
                             <ul class="list-group list-group-flush">
-                                <li class="list-group-item">Brinquedos sensoriais</li>
-                                <li class="list-group-item">Jogos de quebra-cabeça</li>
-                                <li class="list-group-item">Livros interativos</li>
-                                <li class="list-group-item">Kit de arte</li>
-                                <li class="list-group-item">Jogos cooperativos</li>
-                                <li class="list-group-item">Livros para leitura conjunta</li>
-                                <li class="list-group-item">Caixa de Areia</li>
+                                <li class="list-group-item">Novos Brinquedos sensoriais</li>
+                                <li class="list-group-item">Novos Jogos de quebra-cabeça</li>
+                                <li class="list-group-item">Novos Livros interativos</li>
+                                <li class="list-group-item">Novos Kits de arte</li>
+                                <li class="list-group-item">Novos Jogos cooperativos</li>
+                                <li class="list-group-item">Novos Livros para leitura conjunta</li>
+                                <li class="list-group-item">Nova Caixa de Areia</li>
                             </ul>
                             <div class="card-body">
                                 <a href="https://github.com/kauaenrico/UNISAL_Proj_5" class="card-link">Github link</a>
