6137d43f93f65f1d0efc41ebd830f2e26b134fc2
LukeBelatine <luke.belatine@yahoo.com.br>
2024-05-03 19:23:39 -0300

    header

--- src/header.php
+++ src/header.php
@@ -11,8 +11,12 @@
           <a class="nav-link active" href="index.php?page=dashboard">Pagina Inicial<span class="visually-hidden">(current)</span>
           </a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pacientes</a>
+        <li class="nav-item dropdown1">
+          <a  class="nav-link" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Paciente</a>
+          <div class= "dropdown-menu">
+            <a class= "dropdown-item" href= "#">Cadastrar paciente</a>
+            <a class= "dropdown-item" href= "#">Lista de pacientes</a>
+          </div>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Agenda</a>
