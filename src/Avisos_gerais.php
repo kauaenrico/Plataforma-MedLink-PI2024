630a1c970b9dcbdf536211c9910783227c056633
Kauã Enrico <kaua.altran@outlook.com>
2024-05-06 19:32:20 -0300

    Update Avisos_gerais.php

--- src/Avisos_gerais.php
+++ src/Avisos_gerais.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,7 +7,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
-<body>
+<body> -->
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-8">
@@ -33,5 +33,5 @@
 
     <!-- Bootstrap JS, optional -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-</html>
+<!-- </body>
+</html> -->
