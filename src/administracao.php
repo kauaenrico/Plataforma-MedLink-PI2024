b66bea76c268877eda657a22302b2c7e1ad455e6
LukeBelatine <luke.belatine@yahoo.com.br>
2024-05-07 20:42:33 -0300

    Update administracao.php

--- src/administracao.php
+++ src/administracao.php
@@ -36,12 +36,6 @@
                 <button class="btn btn-primary mt-2">Pesquisar Terapeuta</button>
             </div>
             <a href="#" class="btn btn-secondary">Lista de Terapeutas</a>
-            <div class="col-md-4">
-            <div class="mb-3">
-                <input class="form-control" type="search" placeholder="Pesquisar Paciente">
-                <button class="btn btn-primary mt-2">Pesquisar Paciente</button>
-            </div>
-            <a href="#" class="btn btn-secondary">Cadastrar novo paciente</a>
             <ul class="list-group mt-3">
                 <!-- Lista de pacientes fictícios -->
                 <li class="list-group-item d-flex justify-content-between align-items-center">Camily R. <span><button class="btn btn-primary btn-sm me-2">Editar</button><button class="btn btn-danger btn-sm">Apagar</button></span></li>
