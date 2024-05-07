cacb00be413feeb5b59554900ee031a39cd1216d
LukeBelatine <luke.belatine@yahoo.com.br>
2024-05-07 20:44:32 -0300

    Update administracao.php

--- src/administracao.php
+++ src/administracao.php
@@ -37,7 +37,7 @@
             </div>
             <a href="#" class="btn btn-secondary">Lista de Terapeutas</a>
             <ul class="list-group mt-3">
-                <!-- Lista de pacientes fictícios -->
+                <!-- Lista de terapeutas fictícios -->
                 <li class="list-group-item d-flex justify-content-between align-items-center">Camily R. <span><button class="btn btn-primary btn-sm me-2">Editar</button><button class="btn btn-danger btn-sm">Apagar</button></span></li>
                 <li class="list-group-item d-flex justify-content-between align-items-center">Maria S. <span><button class="btn btn-primary btn-sm me-2">Editar</button><button class="btn btn-danger btn-sm">Apagar</button></span></li>
                 <li class="list-group-item d-flex justify-content-between align-items-center">Carlos O. <span><button class="btn btn-primary btn-sm me-2">Editar</button><button class="btn btn-danger btn-sm">Apagar</button></span></li>
@@ -45,9 +45,7 @@
                 <li class="list-group-item d-flex justify-content-between align-items-center">Gabriel G. <span><button class="btn btn-primary btn-sm me-2">Editar</button><button class="btn btn-danger btn-sm">Apagar</button></span></li>
                 <li class="list-group-item d-flex justify-content-between align-items-center">Letícia B. <span><button class="btn btn-primary btn-sm me-2">Editar</button><button class="btn btn-danger btn-sm">Apagar</button></span></li>
             </ul>
-             </div>
         </div>
-        
 
         <!-- Coluna da direita -->
         <div class="col-md-4">
