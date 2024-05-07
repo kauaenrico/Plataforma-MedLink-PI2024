9d0009ab00ae936acb0063b55700757754259cb7
LukeBelatine <luke.belatine@yahoo.com.br>
2024-05-06 22:05:47 -0300

    Update cadpaciente.php

--- src/cadpaciente.php
+++ src/cadpaciente.php
@@ -2,8 +2,8 @@
         .col-md-6 {
             border-left: 20px;
             border-right: 20px;
-            margin-left: -10px;
-            margin-right: -10px;
+            margin-left: 10px;
+            margin-right: 10px;
         }
     </style>
 
