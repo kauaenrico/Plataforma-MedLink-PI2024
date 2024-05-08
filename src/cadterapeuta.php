635802f4dfea6c2c175c7e55eb416f7930c5211d
LukeBelatine <luke.belatine@yahoo.com.br>
2024-05-07 21:17:58 -0300

    Update cadterapeuta.php

--- src/cadterapeuta.php
+++ src/cadterapeuta.php
@@ -1,55 +1,66 @@
-<style>
-    .body {
-        border-left: 15px;
-        border-right: 15px;
-        margin-left: 15px;
-        margin-right: 15px;
-    }
-</style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://bootswatch.com/5/minty/bootstrap.min.css" crossorigin="anonymous">
+    <title>Cadastrar novo terapeuta</title>
+</head>
+<body>
 
-<form data-bitwarden-watching="1">
-    <fieldset>
-        <div class="body">
-            <legend>Cadastrar novo terapeuta</legend>
-            <div class="row">
-                <div class="col-md-6">
-                    <label for="nome" class="form-label mt-3">Nome</label>
-                    <input type="text" class="form-control" id="nome" placeholder="Nome">
+<div class="container-fluid mt-5">
+    <form data-bitwarden-watching="1">
+        <fieldset>
+            <div class="body">
+                <legend>Cadastrar novo terapeuta</legend>
+                <div class="row">
+                    <div class="col-md-6">
+                        <label for="cpf" class="form-label mt-3">CPF</label>
+                        <input type="text" class="form-control" id="cpf" placeholder="CPF">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="nome" class="form-label mt-3">Nome</label>
+                        <input type="text" class="form-control" id="nome" placeholder="Nome">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="departamento" class="form-label mt-3">Departamento de atuação</label>
+                        <input type="text" class="form-control" id="departamento" placeholder="Departamento">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="telefone" class="form-label mt-3">Telefone</label>
+                        <input type="tel" class="form-control" id="telefone" placeholder="Telefone">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="email" class="form-label mt-3">Email</label>
+                        <input type="email" class="form-control" id="email" placeholder="Email">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="status" class="form-label mt-3">Status</label>
+                        <select class="form-select" id="status">
+                            <option selected>Selecione...</option>
+                            <option value="ativo">Ativo</option>
+                            <option value="inativo">Inativo</option>
+                        </select>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="senha" class="form-label mt-3">Senha</label>
+                        <input type="password" class="form-control" id="senha" placeholder="Senha">
+                    </div>
+                    <div class="col-md-12">
+                        <label for="gestor" class="form-label mt-3">Gestor Imediato</label>
+                        <input type="text" class="form-control" id="gestor" placeholder="Gestor Imediato">
+                    </div>
+                    <div class="col-md-12">
+                        <label for="observacoes" class="form-label mt-3">Observações</label>
+                        <textarea class="form-control" id="observacoes" rows="3" placeholder="Observações"></textarea>
+                    </div>
                 </div>
-                <div class="col-md-6">
-                    <label for="Identificador" class="form-label mt-3">ID</label>
-                    <input type="Numero" class="form-control" id="ID">
-                </div>
-                <div class="col-md-6">
-                    <label for="nascimento" class="form-label mt-3">Data de Nascimento</label>
-                    <input type="date" class="form-control" id="nascimento">
-                </div>
-                <div class="col-md-6">
-                    <label for="telefone" class="form-label mt-3">Telefone</label>
-                    <input type="tel" class="form-control" id="telefone" placeholder="Telefone">
-                </div>
-                <div class="col-md-6">
-                    <label for="cargo" class="form-label mt-3">Cargo</label>
-                    <input type="text" class="form-control" id="cargo" placeholder="Cargo">
-                </div>
-                <div class="col-md-6">
-                    <label for="especializacao" class="form-label mt-3">Especialização</label>
-                    <input type="text" class="form-control" id="especializacao" placeholder="Especialização">
-                </div>
-                <div class="col-md-12">
-                    <label for="observacoes" class="form-label mt-3">Observações</label>
-                    <textarea class="form-control" id="observacoes" rows="3" placeholder="Observações"></textarea>
-                </div>
-                <div class="col-md-6">
-                    <label for="status" class="form-label mt-3">Status</label>
-                    <select class="form-select" id="status">
-                        <option selected>Selecione...</option>
-                        <option value="ativo">Ativo</option>
-                        <option value="inativo">Inativo</option>
-                    </select>
-                </div>
+                <button type="submit" class="btn btn-primary mt-3">Salvar</button>
             </div>
-            <button type="submit" class="btn btn-primary mt-3">Salvar</button>
-        </div>
-    </fieldset>
-</form>
+        </fieldset>
+    </form>
+</div>
+
+</body>
+</html>
