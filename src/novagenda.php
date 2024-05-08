46e0ad84214bd67ddb3e8fb6e05307ebcbafb534
LukeBelatine <luke.belatine@yahoo.com.br>
2024-05-07 21:44:40 -0300

    Update novagenda.php

--- src/novagenda.php
+++ src/novagenda.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://bootswatch.com/5/minty/bootstrap.min.css" crossorigin="anonymous">
-    <title>Novo Agendamento</title>
-</head>
-<body>
-
 <div class="container-fluid mt-5">
     <form data-bitwarden-watching="1">
         <fieldset>
@@ -51,6 +40,3 @@
         </fieldset>
     </form>
 </div>
-
-</body>
-</html>
