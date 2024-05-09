95bb87272ff6bda302f57330233337169f7e68ef
Kauã Enrico <kaua.altran@outlook.com>
2024-05-08 22:02:47 -0300

    ok

--- src/counter.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Caminho para o arquivo de contador
-$counterFile = 'assets/counter.txt';
-$counter = file_get_contents($counterFile);
-$counter++;
-echo "Total de acessos: $counter";
-// Salva o novo valor do contador no arquivo
-file_put_contents($counterFile, $counter);
-?>
