94bcb0fb7a487ca1ef26a4b1245a7f22fb90222d
Kauã Enrico <kaua.altran@outlook.com>
2024-05-15 08:59:59 -0300

    Update materiaisABA.php

--- src/materiaisABA.php
+++ src/materiaisABA.php
@@ -69,4 +69,40 @@
 
 
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<br><h2>Materiais para Tratamento ABA</h2><br>
+<ul id="lista-materiais" class="list-group">
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Material 1
+    <div>
+      <a href="assets/materiais/aba/apontar-cores.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
+      <button type="button" class="btn btn-success imprimir-btn"><i class="fas fa-print"></i> Imprimir</button>
+    </div>
+  </li>
+
+  <!-- Outros itens da lista aqui -->
+
+</ul>
+<!-- 
+<script>
+document.addEventListener("DOMContentLoaded", function() {
+    var botoesImprimir = document.querySelectorAll(".imprimir-btn");
+
+    // Adiciona um evento de clique a cada botão de impressão
+    botoesImprimir.forEach(function(botao) {
+        botao.addEventListener("click", function() {
+            // Obtém o link do PDF associado ao botão de impressão
+            var linkPDF = this.parentElement.querySelector('a').getAttribute('href');
+            // Extrai o nome do arquivo PDF
+            var nomeArquivo = linkPDF.split("/").pop();
+
+            // Abre o PDF em uma nova janela e inicia o processo de impressão
+            var pdfWindow = window.open(linkPDF, "_blank");
+            pdfWindow.onload = function() {
+                pdfWindow.print();
+            };
+        });
+    });
+});
+</script> -->
 
