d1f220a66eb6f6c879c35312d5c3709a6ac26cd4
Kauã Enrico <kaua.altran@outlook.com>
2024-05-16 20:37:13 -0300

    Update avisosgerais.php

--- src/avisosgerais.php
+++ src/avisosgerais.php
@@ -29,29 +29,21 @@
   <div class="container my-4">
     <h2 class="text-center">Avisos Gerais</h2><br>
     <div class="card mb-3 mx-auto" style="max-width: 800px;">
-      <h3 class="card-header">Card header</h3>
+      <h3 class="card-header">Novos brinquedos nas salas!</h3>
       <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <h6 class="card-subtitle text-muted">Support card subtitle</h6>
+        <h5 class="card-title">12/04/2024 às 10h46</h5>
+        <h6 class="card-subtitle text-muted">Por Camilly R.</h6>
       </div>
       <svg xmlns="http://www.w3.org/2000/svg" class="d-block user-select-none" width="100%" height="200" aria-label="Placeholder: Image cap" focusable="false" role="img" preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180" style="font-size:1.125rem;text-anchor:middle">
         <rect width="100%" height="100%" fill="#868e96"></rect>
         <text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>
       </svg>
       <div class="card-body">
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <p class="card-text">Novos jogos e brinquedos que nos possibilitarão um auxílio maior no desenvolvimento de nossos pacientes. Veja exemplos de alguns novos brinquedos que chegaram ao Pequenos Passos.</p>
       </div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">Cras justo odio</li>
-        <li class="list-group-item">Dapibus ac facilisis in</li>
-        <li class="list-group-item">Vestibulum at eros</li>
-      </ul>
       <div class="card-body">
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
-      </div>
-      <div class="card-footer text-muted">
-        2 days ago
+        <a href="#" class="card-link">Curtir</a>
+        <a href="#" class="card-link">Compartilhar</a>
       </div>
     </div>
   </div>
