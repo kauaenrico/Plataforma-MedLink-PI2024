adb33fb82a89c25ef3e188b92b1ad760dabad075
Kauã Enrico <kaua.altran@outlook.com>
2024-05-20 19:18:20 -0300

    quest3

--- src/prova/questao3.php
+++ src/prova/questao3.php
@@ -1,55 +1,45 @@
+<!-- Kaua Enrico Pasti Altran - RA 220019627 -->
 <!-- Desenvolva um Formulário em PHP que seja capaz de receber as informações de um Vendedor:
-- ID
-- Nome
-- Data Nascimento
-- Telefone
-- CPF
-- Endereço
-- Email
-- Data de Admissão
-- Regional
-Os dados devem ser armazenados em uma tabela no Banco de Dados MySQL -->
+(Os dados devem ser armazenados em uma tabela no Banco de Dados MySQL)
+    - ID
+    - Nome
+    - Data Nascimento
+    - Telefone
+    - CPF
+    - Endereço
+    - Email
+    - Data de Admissão
+    - Regional
+ -->
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id = $_POST['id'];
-    $nome = $_POST['nome'];
-    $data_nascimento = $_POST['data_nascimento'];
-    $telefone = $_POST['telefone'];
+    $name = $_POST['name'];
+    $dataNasc = $_POST['dataNasc'];
+    $tel = $_POST['tel'];
     $cpf = $_POST['cpf'];
     $endereco = $_POST['endereco'];
     $email = $_POST['email'];
     $data_admissao = $_POST['data_admissao'];
     $regional = $_POST['regional'];
 
-    // Conexão com o banco de dados
+    // Banco MySQL
     $servername = "localhost";
     $username = "admin";
     $password = "unisal@2024";
     $dbname = "loja";
-
     $conn = new mysqli($servername, $username, $password, $dbname);
-
-    // Verifica se houve erro na conexão
     if ($conn->connect_error) {
         die("Falha na conexão: " . $conn->connect_error);
     }
-
-    // Prepara e executa a consulta SQL
-    $sql = "INSERT INTO vendedores (nome, data_nascimento, telefone, cpf, endereco, email, data_admissao, regional) 
-            VALUES ('$nome', '$data_nascimento', '$telefone', '$cpf', '$endereco', '$email', '$data_admissao', '$regional')";
-
+    $sql = "INSERT INTO vendedores (id, name, dataNasc, tel, cpf, endereco, email, data_admissao, regional) 
+            VALUES ('$id', '$name', '$dataNasc', '$tel', '$cpf', '$endereco', '$email', '$data_admissao', '$regional')";
     if ($conn->query($sql) === TRUE) {
-        echo "Novo vendedor cadastrado com sucesso!";
-    } else {
-        echo "Erro: " . $sql . "<br>" . $conn->error;
-    }
-
-    // Fecha a conexão
+        echo "Novo vendedor cadastrado com sucesso!";} else {echo "Erro: " . $sql . "<br>" . $conn->error;}
     $conn->close();
 } else {
-    echo "Método de requisição inválido.";
-}
+    echo "Método de requisição inválido.";}
 ?>
 
 <!DOCTYPE html>
@@ -64,32 +54,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <form action="questao3.php" method="post">
         <label for="id">ID:</label>
         <input type="number" id="id" name="id" required><br>
-
-        <label for="nome">Nome:</label>
-        <input type="text" id="nome" name="nome" required><br>
-
-        <label for="data_nascimento">Data de Nascimento:</label>
-        <input type="date" id="data_nascimento" name="data_nascimento" required><br>
-
-        <label for="telefone">Telefone:</label>
-        <input type="text" id="telefone" name="telefone" required><br>
-
+        <label for="name">Nome Completo:</label>
+        <input type="text" id="name" name="name" required><br>
+        <label for="dataNasc">Data do Nascimento:</label>
+        <input type="date" id="dataNasc" name="dataNasc" required><br>
+        <label for="tel">telefone:</label>
+        <input type="text" id="tel" name="tel" required><br>
         <label for="cpf">CPF:</label>
         <input type="text" id="cpf" name="cpf" required><br>
-
         <label for="endereco">Endereço:</label>
         <input type="text" id="endereco" name="endereco" required><br>
-
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required><br>
-
-        <label for="data_admissao">Data de Admissão:</label>
+        <label for="data_admissao">Data da Admissão:</label>
         <input type="date" id="data_admissao" name="data_admissao" required><br>
-
         <label for="regional">Regional:</label>
         <input type="text" id="regional" name="regional" required><br>
-
-        <input type="submit" value="Cadastrar">
+        <input type="submit" value="Realizar cadastro">
     </form>
 </body>
 </html>
