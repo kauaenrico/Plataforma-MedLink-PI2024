a6a50b88275542024504a76f96e7cc9ff3f31e95
Kauã Enrico <kaua.altran@outlook.com>
2024-05-23 20:48:17 -0300

    Update materiaisPECS.php

--- src/materiaisPECS.php
+++ src/materiaisPECS.php
@@ -1,9 +1,9 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-<br><h2>Materiais para Tratamento CBT</h2>
-<p>(Terapia Cognitivo-Comportamental)</p><br>
+<br><h2>Materiais para Tratamento PECS</h2>
+<p>(Sistema de Comunicação por Troca de Figuras)</p><br>
 <ul class="list-group">
   <li class="list-group-item d-flex justify-content-between align-items-center">
-    Material 1
+    Material PECS 1
     <div>
       <a href="assets/materiais/aba/apontar-cores.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
       <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
@@ -11,7 +11,7 @@
   </li>
 
   <li class="list-group-item d-flex justify-content-between align-items-center">
-    Material 2
+    Material PECS 2
     <div>
       <a href="assets/materiais/aba/apontar-ouvinte.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
       <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
@@ -19,7 +19,7 @@
   </li>
 
   <li class="list-group-item d-flex justify-content-between align-items-center">
-    Material 2
+    Material PECS 2
     <div>
       <a href="assets/materiais/aba/apontar-qual-encaixa.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
       <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
@@ -27,7 +27,7 @@
   </li>
 
   <li class="list-group-item d-flex justify-content-between align-items-center">
-    Material 4
+    Material PECS 4
     <div>
       <a href="assets/materiais/aba/estimulacao-cognitiva.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
       <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
@@ -35,7 +35,7 @@
   </li>
 
   <li class="list-group-item d-flex justify-content-between align-items-center">
-    Material 5
+    Material PECS 5
     <div>
       <a href="assets/materiais/aba/tato-alfabeto.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
       <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
@@ -43,7 +43,7 @@
   </li>
 
   <li class="list-group-item d-flex justify-content-between align-items-center">
-    Material 6
+    Material PECS 6
     <div>
       <a href="assets/materiais/aba/tato-formas-geometricas.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
       <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
@@ -51,7 +51,7 @@
   </li>
 
   <li class="list-group-item d-flex justify-content-between align-items-center">
-    Material 7
+    Material PECS 7
     <div>
       <a href="assets/materiais/aba/tato-gestos.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
       <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
@@ -59,7 +59,7 @@
   </li>
 
   <li class="list-group-item d-flex justify-content-between align-items-center">
-    Material 8
+    Material PECS 8
     <div>
       <a href="assets/materiais/aba/tato-materiais-escolares.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
       <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
