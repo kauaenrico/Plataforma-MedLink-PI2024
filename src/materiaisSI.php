f12ce48be7cfa2474110fb38c9a874d68f363017
Kauã Enrico <kaua.altran@outlook.com>
2024-05-23 20:44:54 -0300

    criando arquivos materiais

---
+++ src/materiaisSI.php
@@ -0,0 +1,69 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<br><h2>Materiais para Tratamento CBT</h2>
+<p>(Terapia Cognitivo-Comportamental)</p><br>
+<ul class="list-group">
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Material 1
+    <div>
+      <a href="assets/materiais/aba/apontar-cores.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
+      <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
+    </div>
+  </li>
+
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Material 2
+    <div>
+      <a href="assets/materiais/aba/apontar-ouvinte.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
+      <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
+    </div>
+  </li>
+
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Material 2
+    <div>
+      <a href="assets/materiais/aba/apontar-qual-encaixa.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
+      <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
+    </div>
+  </li>
+
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Material 4
+    <div>
+      <a href="assets/materiais/aba/estimulacao-cognitiva.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
+      <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
+    </div>
+  </li>
+
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Material 5
+    <div>
+      <a href="assets/materiais/aba/tato-alfabeto.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
+      <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
+    </div>
+  </li>
+
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Material 6
+    <div>
+      <a href="assets/materiais/aba/tato-formas-geometricas.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
+      <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
+    </div>
+  </li>
+
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Material 7
+    <div>
+      <a href="assets/materiais/aba/tato-gestos.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
+      <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
+    </div>
+  </li>
+
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Material 8
+    <div>
+      <a href="assets/materiais/aba/tato-materiais-escolares.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
+      <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
+    </div>
+  </li>
+
+</ul>
