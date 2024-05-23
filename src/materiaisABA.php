5461234c1b536abd54da405c06e3c888694cdfda
Kauã Enrico <kaua.altran@outlook.com>
2024-05-23 20:34:08 -0300

    Update materiaisABA.php

--- src/materiaisABA.php
+++ src/materiaisABA.php
@@ -2,7 +2,7 @@
 <br><h2>Materiais para Tratamento ABA</h2><br>
 <ul class="list-group">
   <li class="list-group-item d-flex justify-content-between align-items-center">
-    Material 1
+    Apontar Cores
     <div>
       <a href="assets/materiais/aba/apontar-cores.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
       <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
@@ -10,7 +10,7 @@
   </li>
 
   <li class="list-group-item d-flex justify-content-between align-items-center">
-    Material 2
+    Aponte os sons
     <div>
       <a href="assets/materiais/aba/apontar-ouvinte.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
       <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
@@ -18,7 +18,7 @@
   </li>
 
   <li class="list-group-item d-flex justify-content-between align-items-center">
-    Material 2
+    Qual encaixa (Quebra-Cabeças)
     <div>
       <a href="assets/materiais/aba/apontar-qual-encaixa.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
       <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
@@ -26,7 +26,7 @@
   </li>
 
   <li class="list-group-item d-flex justify-content-between align-items-center">
-    Material 2
+    Estimulações Cognitivas
     <div>
       <a href="assets/materiais/aba/estimulacao-cognitiva.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
       <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
@@ -34,7 +34,7 @@
   </li>
 
   <li class="list-group-item d-flex justify-content-between align-items-center">
-    Material 2
+    Tato com Alfabeto
     <div>
       <a href="assets/materiais/aba/tato-alfabeto.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
       <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
@@ -42,7 +42,7 @@
   </li>
 
   <li class="list-group-item d-flex justify-content-between align-items-center">
-    Material 2
+    Tato com Formas Geométricas
     <div>
       <a href="assets/materiais/aba/tato-formas-geometricas.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
       <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
@@ -50,7 +50,7 @@
   </li>
 
   <li class="list-group-item d-flex justify-content-between align-items-center">
-    Material 2
+    Tato com Gestos
     <div>
       <a href="assets/materiais/aba/tato-gestos.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
       <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
@@ -58,7 +58,7 @@
   </li>
 
   <li class="list-group-item d-flex justify-content-between align-items-center">
-    Material 2
+    Tato com Materiais escolares
     <div>
       <a href="assets/materiais/aba/tato-materiais-escolares.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
       <button type="button" class="btn btn-success" onclick="window.print()"><i class="fas fa-print"></i> Imprimir</button>
@@ -66,43 +66,3 @@
   </li>
 
 </ul>
-
-
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-<br><h2>Materiais para Tratamento ABA</h2><br>
-<ul id="lista-materiais" class="list-group">
-  <li class="list-group-item d-flex justify-content-between align-items-center">
-    Material 1
-    <div>
-      <a href="assets/materiais/aba/apontar-cores.pdf" class="btn btn-primary" download><i class="fas fa-download"></i> Download</a>
-      <button type="button" class="btn btn-success imprimir-btn"><i class="fas fa-print"></i> Imprimir</button>
-    </div>
-  </li>
-
-  <!-- Outros itens da lista aqui -->
-
-</ul>
-<!-- 
-<script>
-document.addEventListener("DOMContentLoaded", function() {
-    var botoesImprimir = document.querySelectorAll(".imprimir-btn");
-
-    // Adiciona um evento de clique a cada botão de impressão
-    botoesImprimir.forEach(function(botao) {
-        botao.addEventListener("click", function() {
-            // Obtém o link do PDF associado ao botão de impressão
-            var linkPDF = this.parentElement.querySelector('a').getAttribute('href');
-            // Extrai o nome do arquivo PDF
-            var nomeArquivo = linkPDF.split("/").pop();
-
-            // Abre o PDF em uma nova janela e inicia o processo de impressão
-            var pdfWindow = window.open(linkPDF, "_blank");
-            pdfWindow.onload = function() {
-                pdfWindow.print();
-            };
-        });
-    });
-});
-</script> -->
-
