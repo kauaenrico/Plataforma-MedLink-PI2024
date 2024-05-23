dd67198773842d54d156ec2182fbb53a366eec6e
Kauã Enrico <kaua.altran@outlook.com>
2024-05-23 20:50:10 -0300

    Update materiais.php

--- src/materiais.php
+++ src/materiais.php
@@ -6,11 +6,11 @@
         <p></p><br>
         <div class="list-group">
           <a href= "index.php?page=pmateriais&m=tABA"><button type="button" class="list-group-item list-group-item-action" data-category="categoria1">Tratamento ABA</button></a>
-          <a href= "index.php?page=pmateriais&m=XXXXX"><button type="button" class="list-group-item list-group-item-action" data-category="categoria2">Tratamento PECS</button></a>
-          <a href= "index.php?page=pmateriais&m=XXXXX"><button type="button" class="list-group-item list-group-item-action" data-category="categoria2">Tratamento TEACCH</button></a>
-          <a href= "index.php?page=pmateriais&m=XXXXX"><button type="button" class="list-group-item list-group-item-action" data-category="categoria2">Tratamento RDI</button></a>
-          <a href= "index.php?page=pmateriais&m=XXXXX"><button type="button" class="list-group-item list-group-item-action" data-category="categoria2">Tratamento CBT</button></a>
-          <a href= "index.php?page=pmateriais&m=XXXXX"><button type="button" class="list-group-item list-group-item-action" data-category="categoria2">Tratamento SI</button></a>
+          <a href= "index.php?page=pmateriais&m=tPECS"><button type="button" class="list-group-item list-group-item-action" data-category="categoria2">Tratamento PECS</button></a>
+          <a href= "index.php?page=pmateriais&m=tTEACCH"><button type="button" class="list-group-item list-group-item-action" data-category="categoria2">Tratamento TEACCH</button></a>
+          <a href= "index.php?page=pmateriais&m=tRDI"><button type="button" class="list-group-item list-group-item-action" data-category="categoria2">Tratamento RDI</button></a>
+          <a href= "index.php?page=pmateriais&m=tCBT"><button type="button" class="list-group-item list-group-item-action" data-category="categoria2">Tratamento CBT</button></a>
+          <a href= "index.php?page=pmateriais&m=tSI"><button type="button" class="list-group-item list-group-item-action" data-category="categoria2">Tratamento SI</button></a>
           <!-- Adicione mais botões conforme necessário -->
         </div>
       </div>
@@ -20,7 +20,12 @@
         <?php
     if(isset($_GET["m"])){
         switch($_GET["m"]){
-            case "tABA":      include "materiaisABA.php";        break;    
+            case "tABA":      include "materiaisABA.php";        break;
+            case "tPECS":      include "materiaisPECS.php";        break;
+            case "tTEACCH":      include "materiaisTEACCH.php";        break;
+            case "tRDI":      include "materiaisRDI.php";        break;
+            case "tCBT":      include "materiaisCBT.php";        break;
+            case "tSI":      include "materiaisSI.php";        break;  
             default:                include "error404.php";         break;
         }
     }
