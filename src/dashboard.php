f7107360fc25308f56b95e97394f58cf00e47870
Kauã Enrico <kaua.altran@outlook.com>
2024-06-02 16:39:57 -0300

    avisos na dash

--- src/dashboard.php
+++ src/dashboard.php
@@ -23,34 +23,7 @@
         <!-- Terceira coluna -->
         <div class="col-md-4">
             <div class="container mt-5">
-                <div class="card mb-3">
-                    <h2 class="card-header">Avisos gerais</h2>
-                    <div class="card-body">
-                        <h4 class="card-title">Novos brinquedos nas salas!</h4>
-                        <h6 class="card-subtitle text-muted">Novas experiências para novos resultados!</h6>
-                    </div>
-                    <img src="assets/img/Imagemclinica.jpeg" width="100%" height="200" alt="Imagem da clínica" style="object-fit: cover;">
-                    <div class="card-body">
-                        <p class="card-text">Novos jogos e brinquedos que nos possibilitarão um auxílio maior no desenvolvimento de nossos pacientes. Veja exemplos de alguns novos brinquedos que chegaram ao Pequenos Passos</p>
-                        <!-- Link "Saiba mais" -->
-                        <a href="#" class="btn btn-primary" id="saibaMaisBtn">Saiba mais</a>
-                        <!-- Div oculta com informações adicionais -->
-                        <div id="infoAdicionais" style="display: none;">
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item">Novos Brinquedos sensoriais</li>
-                                <li class="list-group-item">Novos Jogos de quebra-cabeça</li>
-                                <li class="list-group-item">Novos Livros interativos</li>
-                                <li class="list-group-item">Novos Kits de arte</li>
-                                <li class="list-group-item">Novos Jogos cooperativos</li>
-                                <li class="list-group-item">Novos Livros para leitura conjunta</li>
-                                <li class="list-group-item">Nova Caixa de Areia</li>
-                            </ul>
-                            <div class="card-footer text-muted">
-                                2 dias atrás
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <?php include "dashboard/avisosDash.php"; ?>
             </div>
         </div>
     </div>
