4c5be78754ea21c049d95a3b3d2d253bc2df5ea5
Kauã Enrico <kaua.altran@outlook.com>
2024-06-02 22:04:47 -0300

    Update login.php

--- src/login.php
+++ src/login.php
@@ -8,7 +8,7 @@ header("Location: index.php?page=pdashboard");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://bootswatch.com/5/zephyr/bootstrap.min.css">
     <style>
         body {
             background-color: #d6e7df;
