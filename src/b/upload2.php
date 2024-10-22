147d1d873bf980885d1eb6759b847a6b7fabd7b9
Kauã Enrico <kaua.altran@outlook.com>
2024-10-21 22:07:48 -0300

    teste novo

--- src/b/upload2.php
+++ src/b/upload2.php
@@ -1,20 +1,21 @@
 <html>
-
 <body>
-    <?php
-    /* Get the name of the file uploaded to Apache */
-    $filename = $_FILES['file']['name'];
+<?php
 
-    /* Prepare to save the file upload to the upload folder */
-    $location = "/projeto/medlink/data/uploads/" . $filename;
+/* Get the name of the file uploaded to Apache */
+$filename = $_FILES['file']['name'];
 
-    /* Permanently save the file upload to the upload folder */
-    if (move_uploaded_file($_FILES['file']['tmp_name'], $location)) {
-        echo '<p>The HTML5 and php file upload was a success!</p>';
-    } else {
-        echo '<p>The php and HTML5 file upload failed.</p>';
-    }
-    ?>
+/* Prepare to save the file upload to the upload folder */
+$location = "/projeto/medlink/data/uploads/".$filename;
+
+/* Permanently save the file upload to the upload folder */
+if (move_uploaded_file($_FILES['file']['tmp_name'], $location)) { 
+    echo '<p>The HTML5 and PHP file upload was a success!</p>'; 
+} else { 
+    echo '<p>The PHP and HTML5 file upload failed.</p>'; 
+    echo '<p>Error: ' . $_FILES['file']['error'] . '</p>'; // Exibe o erro do upload
+}
+
+?>
 </body>
-
 </html>
